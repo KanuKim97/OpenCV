57d461bf585690e7ad8d05e703cd15bdecbbe966
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2020-11-16 00:27:17 +0900

    Update OpenCV_Picture.py

--- OpenCV_Picture.py
+++ OpenCV_Picture.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('E:\python\OpenCV\sample.jpg', 1)
+img = cv2.imread('E:\python\OpenCV\sample.jpg', 1) //Upload your sample img
 
 cv2.imshow('Test Img', img)
 cv2.waitKey(0)
